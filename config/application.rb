87eb289625d0f56d5b70a102a84b7c7608b89060
yodit93 <yoditabebe9391@gmail.com>
2024-02-13 17:00:31 +0300

    Update application.rb file

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module CarReservationApi
 
     # Configuration for the application, engines, and railties goes here.
     #
-    config.api_only = true
+    config.api_only = false
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
