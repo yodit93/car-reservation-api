3e32a1bdfbdfcb42771c504cb8c3da6e44040c75
yodit93 <yoditabebe9391@gmail.com>
2024-02-13 12:30:04 +0300

    Update api-only mode

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,10 @@ Bundler.require(*Rails.groups)
 module CarReservationApi
   class Application < Rails::Application
     config.load_defaults 7.0
-    config.assets.enabled = false
+
     # Configuration for the application, engines, and railties goes here.
     #
-    config.api_only = false
+    config.api_only = true
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
